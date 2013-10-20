febf5225230ebcdefbcb9e5dad8ba8fbb85b0517
Vasil Rangelov <boen.robot@gmail.com>
2013-10-20 22:11:58 +0300

    Added PHP and a few other project types as affected by this module.

--- src/main/java/de/markiewb/netbeans/plugins/readmeinprojectview/ReadmeNodeFactory.java
+++ src/main/java/de/markiewb/netbeans/plugins/readmeinprojectview/ReadmeNodeFactory.java
@@ -30,7 +30,27 @@ import org.openide.loaders.DataObjectNotFoundException;
 import org.openide.nodes.Node;
 
 @NodeFactory.Registration(projectType = {
-    "org-netbeans-modules-ant-freeform", "org-netbeans-modules-apisupport-project", "org-netbeans-modules-apisupport-project-suite", "org-netbeans-modules-apisupport-project-suite-jnlp", "org-netbeans-modules-apisupport-project-suite-osgi", "org-netbeans-modules-apisupport-project-suite-package", "org-netbeans-modules-autoproject", "org-netbeans-modules-j2ee-clientproject", "org-netbeans-modules-j2ee-earproject", "org-netbeans-modules-j2ee-ejbjarproject", "org-netbeans-modules-java-j2seproject", "org-netbeans-modules-maven", "org-netbeans-modules-web-project"
+    "org-netbeans-modules-ant-freeform",
+    "org-netbeans-modules-apisupport-project",
+    "org-netbeans-modules-apisupport-project-suite",
+    "org-netbeans-modules-apisupport-project-suite-jnlp",
+    "org-netbeans-modules-apisupport-project-suite-osgi",
+    "org-netbeans-modules-apisupport-project-suite-package",
+    "org-netbeans-modules-autoproject",
+    "org-netbeans-modules-bpel-project",
+    "org-netbeans-modules-j2ee-clientproject",
+    "org-netbeans-modules-j2ee-earproject",
+    "org-netbeans-modules-j2ee-ejbjarproject",
+    "org-netbeans-modules-java-j2seproject",
+    "org-netbeans-modules-javacard-project",
+    "org-netbeans-modules-javaee-project",
+    "org-netbeans-modules-javafx2-project",
+    "org-netbeans-modules-maven",
+    "org-netbeans-modules-php-project",
+    "org-netbeans-modules-ruby-project",
+    "org-netbeans-modules-sql-project",
+    "org-netbeans-modules-web-project",
+    "org-netbeans-modules-xslt-project"
 },
         position = 9000)
 public class ReadmeNodeFactory implements NodeFactory {
